ff57b1a3db0f3b66e7014fe3c0fe6fd770112dae
Joaquim <38539884+jocasine2@users.noreply.github.com>
2022-07-20 12:25:35 -0300

    Update start_development.sh

--- start_development.sh
+++ start_development.sh
@@ -3,3 +3,23 @@ docker-compose up -d
 
 #adicionando permissão de edição para o usuário atual
 sudo chown -R $USER:$USER tmp 
+
+
+#criando o app
+docker-compose run api rails new app --api
+docker-compose run api gem install bundler:2.3.13
+docker-compose run api bundle install
+
+#para criar um app em modo API
+docker-compose run api rails new app --api
+
+#iniciando banco de dados
+sudo docker-compose up -d postgres
+
+#criando o banco de dados
+docker-compose run api rails db:create
+docker-compose run api rails db:migrate
+docker-compose run api rails db:seed
+
+#inicia api para mostrando os logs no terminal
+sudo docker-compose run --service-ports api
