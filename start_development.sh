a35726c5a704f78b19e6da61671ce9dfb22980c2
Joaquim <38539884+jocasine2@users.noreply.github.com>
2022-07-26 11:18:33 -0300

    Update start_development.sh

--- start_development.sh
+++ start_development.sh
@@ -12,14 +12,22 @@ sudo chmod 777 -R /home/joaquim/Sistemas/desafio_dev_rails_angular/quickstart_ra
 
 
 #para remover o app criado
-# sudo rm -rf bin
-# sudo rm -rf config
-# sudo rm -rf db
-# sudo rm -rf lib
-# sudo rm -rf log
-# sudo rm -rf public
-# sudo rm -rf storage
-# sudo rm -rf test
-# sudo rm -rf tmp
-# sudo rm -rf vendor
-# sudo rm -rf app
+#sudo rm -rf bin
+#sudo rm -rf config
+#sudo rm -rf db
+#sudo rm -rf lib
+#sudo rm -rf log
+#sudo rm -rf public
+#sudo rm -rf storage
+#sudo rm -rf test
+#sudo rm -rf tmp
+#sudo rm -rf vendor
+#sudo rm -rf app
+#sudo rm -rf .gitattributes
+#sudo rm -rf config.ru
+#sudo rm -rf Gemfile.lock 
+#sudo rm -rf package.json
+#sudo rm -rf Rakefile
+#sudo rm -rf .ruby-version
+#sudo rm -rf Gemfile
+
