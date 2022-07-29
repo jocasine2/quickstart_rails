414867d212b1ac40db3269659408e5cb08fc6cbe
joaquim.neto <joaquim.neto@ati.to.gov.br>
2022-07-29 13:56:47 -0300

    🚑 Corrige caminho para o functions.sh

--- start_development.sh
+++ start_development.sh
@@ -1,12 +1,13 @@
 #adicionando funções ao bash
-source docker-compose/functions.sh
+my_dir="$(pwd)"
+$my_dir/docker-compose/functions.sh
 
 #iniciando banco de dados
 sudo docker-compose up -d
 
 #corrigindo permissões
-# sudo chmod 777 "$(pwd)/config/master.key"
-# sudo chmod 777 -R "$(pwd)/tmp/db"
+sudo chmod 777 "$(pwd)/config/master.key"
+sudo chmod 777 -R "$(pwd)/tmp/db"
 
 docker-compose run app sed -i "7c CMD exec docker-compose/start.sh" Dockerfile
 
