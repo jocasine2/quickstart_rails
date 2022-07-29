c21185dbf99c9f3ff2740796adaeaec1cfe09518
joaquim.neto <joaquim.neto@ati.to.gov.br>
2022-07-29 13:45:08 -0300

    🚧 Adiciona funções ao bash

---
+++ docker-compose/functions.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+#funções uteis
+
+function app(){
+    docker-compose run app $@
+}
+
+function new_app(){
+    app rails new ../app
+}
+
+function app_reset(){
+    permissions_update
+    remove_app
+    new_app
+}
+
+function app_scaffold_api(){
+    docker-compose run app rails g scaffold $@ --api
+}
+
+function bd(){
+    docker-compose run bd $@
+}
+
+function remove_app(){
+    permissions_update
+
+    #para remover o app criado 
+    rm -rf bin 
+    rm -rf config 
+    rm -rf db 
+    rm -rf lib 
+    rm -rf log 
+    rm -rf public 
+    rm -rf storage 
+    rm -rf test 
+    rm -rf tmp 
+    rm -rf vendor 
+    rm -rf app 
+    rm -rf .gitattributes 
+    rm -rf config.ru 
+    rm -rf Gemfile.lock  
+    rm -rf package.json 
+    rm -rf Rakefile 
+    rm -rf .ruby-version 
+    rm -rf Gemfile
+}
+
+function migrate(){
+    app rails db:drop db:create db:migrate db:seed
+}
+
+function permissions_update(){
+    sudo chown -R $USER:$USER .
+}
+
+function prune(){
+    docker system prune -a -f
+}
+
+function build_project(){
+    app_reset
+
+    app_scaffold_api unit name:string
+    app_scaffold_api localization longitude:string latitude:string
+    app_scaffold_api store name:string localization:references
+    app_scaffold_api list name:string date_time:date store:references
+    app_scaffold_api item name:string list:references
+    app_scaffold_api itemlist item:references list:references default:boolean unit:references
+    app_scaffold_api priceperunitofmeasure quantity:float unit:references
+    app_scaffold_api product description:string unit:references packagingquantity:float price:float
+    app_scaffold_api productitem item:references product:references
+    app_scaffold_api storeproduct store:references product:references
+
+    docker-compose up -d
+
+    app rails db:create
+    app rails db:migrate
+}
+
+function destroy_project(){
+    remove_app
+    docker-compose down
+    prune
+}
--- start_development.sh
+++ start_development.sh
@@ -1,32 +1,14 @@
+#adicionando funções ao bash
+source docker-compose/functions.sh
+
 #iniciando banco de dados
 sudo docker-compose up -d
 
 #corrigindo permissões
-sudo chmod 777 "$(pwd)/config/master.key"
-sudo chmod 777 -R "$(pwd)/tmp/db"
+# sudo chmod 777 "$(pwd)/config/master.key"
+# sudo chmod 777 -R "$(pwd)/tmp/db"
 
 docker-compose run app sed -i "7c CMD exec docker-compose/start.sh" Dockerfile
 
 #reiniciando containers para aplicar as alterações
 sudo chmod 777 -R "$(pwd)/tmp/db"
-
-
-#para remover o app criado
-#sudo rm -rf bin
-#sudo rm -rf config
-#sudo rm -rf db
-#sudo rm -rf lib
-#sudo rm -rf log
-#sudo rm -rf public
-#sudo rm -rf storage
-#sudo rm -rf test
-#sudo rm -rf tmp
-#sudo rm -rf vendor
-#sudo rm -rf app
-#sudo rm -rf .gitattributes
-#sudo rm -rf config.ru
-#sudo rm -rf Gemfile.lock 
-#sudo rm -rf package.json
-#sudo rm -rf Rakefile
-#sudo rm -rf .ruby-version
-#sudo rm -rf Gemfile
