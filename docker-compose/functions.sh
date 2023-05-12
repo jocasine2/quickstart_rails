c8988ef99090add89c10ef0f906f25846cd4a22b
Joaquim <38539884+jocasine2@users.noreply.github.com>
2023-05-12 16:30:52 -0300

    Update functions.sh
    Adiciona instalação do docker e docker-compose ao projeto

--- docker-compose/functions.sh
+++ docker-compose/functions.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# Função para instalar Docker e Docker Compose
+install_docker_compose() {
+    # Verifica se o Docker está instalado
+    if ! [ -x "$(command -v docker)" ]; then
+        echo "Instalando Docker"
+        # Instala Docker
+        curl -fsSL https://get.docker.com -o get-docker.sh
+        sudo sh get-docker.sh
+        sudo usermod -aG docker $USER
+        # Inicia serviço do Docker
+        sudo systemctl start docker
+        sudo systemctl enable docker
+        # Remove script de instalação
+        rm get-docker.sh
+    else
+        echo "Docker já está instalado"
+    fi
+    
+    # Verifica se o Docker Compose está instalado
+    if ! [ -x "$(command -v docker-compose)" ]; then
+        echo "Instalando Docker Compose"
+        # Instala Docker Compose
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+    else
+        echo "Docker Compose já está instalado"
+    fi
+}
+
+# Chama a função de instalação do Docker e Docker Compose
+install_docker_compose
+
 #funções uteis
 function getEnv(){
     eval "$(
